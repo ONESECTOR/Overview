6dc1b62308381aefe4260a33798fc3bbd2f492af
SECTOR <75115941+ONESECTOR@users.noreply.github.com>
2024-05-10 01:37:28 +0300

    review

--- app/src/main/java/com/sector/overview/ui/reviews/detail/ReviewDetailFragment.kt
+++ app/src/main/java/com/sector/overview/ui/reviews/detail/ReviewDetailFragment.kt
@@ -3,6 +3,7 @@ package com.sector.overview.ui.reviews.detail
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.sector.overview.R
@@ -31,10 +32,35 @@ class ReviewDetailFragment : Fragment(R.layout.fragment_review_detail) {
             state = ::handleState,
             sideEffect = ::handleSideEffect
         )
+
+        viewBinding.toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
     }
 
     private fun handleState(state: ReviewDetailViewState) {
+        state.review?.let { review ->
+            viewBinding.toolbar.title = review.movieName
 
+            viewBinding.tvShortDescription.text = review.shortDescription
+            viewBinding.tvReview.text = review.reviewText
+
+            viewBinding.tvPlotValue.text = getString(R.string.review_value_max_20, review.plotRating)
+            viewBinding.tvActingPerformanceValue.text = getString(R.string.review_value_max_15, review.actingPerformance)
+            viewBinding.tvDirectionValue.text = getString(R.string.review_value_max_15, review.direction)
+            viewBinding.tvArtisticDesignValue.text = getString(R.string.review_value_max_15, review.artisticDesign)
+            viewBinding.tvEditingValue.text = getString(R.string.review_value_max_10, review.editing)
+            viewBinding.tvMusicAndSoundDesignValue.text = getString(R.string.review_value_max_10, review.musicAndSoundDesign)
+            viewBinding.tvOriginalityValue.text = getString(R.string.review_value_max_10, review.originality)
+            viewBinding.tvEmotionalImpactValue.text = getString(R.string.review_value_max_5, review.emotionalImpact)
+
+            viewBinding.sliderPlot.value = review.plotRating?.toFloat() ?: 0f
+            viewBinding.sliderActingPerformance.value = review.actingPerformance?.toFloat() ?: 0f
+            viewBinding.sliderDirection.value = review.direction?.toFloat() ?: 0f
+            viewBinding.sliderArtisticDesign.value = review.artisticDesign?.toFloat() ?: 0f
+            viewBinding.sliderEditing.value = review.editing?.toFloat() ?: 0f
+            viewBinding.sliderMusicAndSoundDesign.value = review.musicAndSoundDesign?.toFloat() ?: 0f
+            viewBinding.sliderOriginality.value = review.originality?.toFloat() ?: 0f
+            viewBinding.sliderEmotionalImpact.value = review.emotionalImpact?.toFloat() ?: 0f
+        }
     }
 
     private fun handleSideEffect(sideEffect: ReviewDetailSideEffect) {
--- app/src/main/java/com/sector/overview/ui/reviews/detail/ReviewDetailViewModel.kt
+++ app/src/main/java/com/sector/overview/ui/reviews/detail/ReviewDetailViewModel.kt
@@ -2,6 +2,8 @@ package com.sector.overview.ui.reviews.detail
 
 import com.sector.domain.entity.firebase.Review
 import com.sector.ui.viewmodel.BaseViewModel
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
 
 internal class ReviewDetailViewModel(
     private val review: Review
@@ -11,13 +13,15 @@ internal class ReviewDetailViewModel(
         initialize()
     }
 
-    private fun initialize() {
-
+    private fun initialize() = intent {
+        reduce {
+            state.copy(review = review)
+        }
     }
 }
 
 internal data class ReviewDetailViewState(
-    val name: String = ""
+    val review: Review? = null
 )
 
 internal sealed class ReviewDetailSideEffect {
