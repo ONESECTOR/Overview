81206321ff9b8040d3b575968efa680f1bafccbd
ONESECTOR <onesector.dev@gmail.com>
2024-07-26 05:29:46 +0300

    git test success 2

--- app/src/main/java/com/sector/overview/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/sector/overview/ui/home/HomeViewModel.kt
@@ -34,13 +34,6 @@ internal class HomeViewModel(
         getMovies()
         getPopularReviews()
         getReviewsWithBestPlot()
-        println(1)
-        println(2)
-        println(3)
-        println(4)
-        println(5)
-        println(6)
-        println(7)
     }
 
     private fun setGreeting() = intent {
